dea20fd7e2146be06d0bafba79f9f808eb8cf63d
Rianne Lim <rianne.limje@gmail.com>
2025-05-10 00:13:19 +0800

    comment out sign in first

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,9 +26,9 @@ export default function Header() {
           </Link>
         </nav>
         {/* im thinking if i n</div>eed a sign in */}
-        <Button variant="outline" className="border-red-500 text-red-500 hover:bg-red-950">
+        {/* <Button variant="outline" className="border-red-500 text-red-500 hover:bg-red-950">
           Sign In 
-        </Button>
+        </Button> */}
       </div>
     </header>
   )
